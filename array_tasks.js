cf1d578e244099860d9a59643bf312b70ec95479
BoscoMurray <rossamurray@gmail.com>
2017-07-26 17:28:29 +0100

    initial commit, 1st test passed

---
+++ array_tasks.js
@@ -0,0 +1,44 @@
+var arrayTasks = {
+
+	concat: function (arr1, arr2) {
+		var tempArray = [];
+		for (var item of arr1) {
+			tempArray.push(item);
+		}
+		for (var item of arr2) {
+			tempArray.push(item);
+		}
+		return tempArray;
+	},
+
+	// insertAt: function (arr, itemToAdd, index) {
+		
+	// },
+
+	// square: function (arr) {
+		
+	// },
+
+	// sum: function (arr) {
+		
+	// },
+
+	// findDuplicates: function (arr) {
+		
+	// },
+
+	// removeAndClone: function (arr, valueToRemove) {
+		
+	// },
+
+	// findIndexesOf: function (arr, itemToFind) {
+		
+	// },
+
+	// sumOfAllEvenNumbersSquared: function (arr) {
+		
+	// }
+
+}
+
+module.exports = arrayTasks
